0c55b9d2e8ad5409802d563e50804d0ddd95b753
Dennis Dinh <dennisdinhhung@gmail.com>
2021-10-24 20:17:19 +0700

    slightly update manage.php

--- main/manage.php
+++ main/manage.php
@@ -11,7 +11,7 @@
     <!--Bootstrap link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- external css link-->
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="../styles/styles.css">
     <!--Jquery Link-->
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
@@ -19,8 +19,18 @@
     <div>
         <?php include('static/navbar.php'); ?>
     </div>
-    <div>
+    <div class="title">
         <h1>Management</h1>
+    </div>
+    <div class="">
+        <button>Add data</button>
+        <button>Add schedules</button>
+    </div>
+    <div class="list-doctors">
+        <!--Insert list of doctors here-->
+        <?php
+            
+        ?>
     </div>
 </body>
 </html>
