71dfc1ac331a910ecb4405c40e14422d75d45755
dennisdinhhung <dennisdinhhung@mgmail.com>
2021-10-26 15:08:30 +0700

    fixing header

--- main/add-doctor.php
+++ main/add-doctor.php
@@ -4,12 +4,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Group XX - Hospital Management</title>
+    <!--Jquery Link-->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     <!--Bootstrap link-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- external css link-->
     <link rel="stylesheet" href="../styles/styles.css">
-    <!--Jquery Link-->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!--fontawesome for icons-->
+    <script src="https://kit.fontawesome.com/eb021599d5.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div>
---
+++ main/add-room.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Group XX - Hospital Management</title>
+    <!--Jquery Link-->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!--Bootstrap link-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- external css link-->
+    <link rel="stylesheet" href="../styles/styles.css">
+    <!--fontawesome for icons-->
+    <script src="https://kit.fontawesome.com/eb021599d5.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div>
+        <?php include('static/navbar.php'); ?>
+    </div>
+    <div class="title">
+        <h1>Add room</h1>
+    </div>
+</body>
+</html>
