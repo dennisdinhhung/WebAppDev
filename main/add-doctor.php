19834367b94c8cf7a0188b7e3d8cbf99d0c235b2
dennisdinhhung <dennisdinhhung@mgmail.com>
2021-10-27 14:38:56 +0700

    BUG: submit button cause blank screen

--- main/add-doctor.php
+++ main/add-doctor.php
@@ -1,3 +1,24 @@
+<?php
+    session_start();
+    error_reporting(0);
+    include ('include/config.php');
+    
+    if(isset($_POST['submit'])){
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $field = $_POST['field'];
+        $email = $_POST['email'];
+
+        
+        $new_doc_id_sql = mysqli_query($conn, "SELECT MAX(id) AS max_id FROM doctors;");
+        $new_doc_id = mysqli_fetch_array($new_doc_id_sql);
+        $id = $new_doc_id['max_id'];
+    
+        mysqli_query($conn, "INSERT INTO doctors (id,first_name,last_name,field,email)
+        VALUES ($id,$first_name, $last_name, $field, $email);");
+    }
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -20,6 +41,53 @@
     </div>
     <div class="title">
         <h1>Add doctor</h1>
+    </div>
+    <div class="app">
+        <!--TITLE-->
+        <div class="list-title margin">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                    <li class="breadcrumb-item"><a href="index.php">Doctor</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Add doctor</li>
+                </ol>
+            </nav>
+        </div>
+
+        <!--BODY-->
+        <div class="div-body">
+            <div>
+                <form autocomplete="off" role="form" name="adddoc" method="post" onsubmit="return valid();">
+                    <div class="form-section">
+                        <label for="first_name">First Name</label>
+                        <input type="text" name="first_name" class="form-control" 
+                            placeholder="Enter first name" >
+                    </div>
+
+                    <div class="form-section">
+                        <label for="last_name">Last Name</label>
+                        <input type="text" name="last_name" class="form-control" 
+                            placeholder="Enter last name" >
+                    </div>
+
+                    <div class="form-section">
+                        <label for="last_name">Field</label>
+                        <input type="text" name="field" class="form-control" 
+                            placeholder="Enter field profession" >
+                    </div>
+
+                    <div class="form-section">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" class="form-control" 
+                            placeholder="Enter email address" >
+                    </div>
+
+                    <button type="submit" name="submit" class="btn btn-primary">
+                        Add
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>
